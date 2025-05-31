051e50719e7cb3a40e39b708d59b7aff997c1070
charbelkhoueiry <rayanamm123@gmail.com>
2025-05-31 19:48:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebase-service-account.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
